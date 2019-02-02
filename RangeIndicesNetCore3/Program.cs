8fb8d5fa2a0e129ecf1407bcb315f7720e099c29
Renícius Pagotto Fostaini <renicius.pagotto@outlook.com>
2019-02-02 13:31:45 -0200

    Initialize

---
+++ RangeIndicesNetCore3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RangeIndicesNetCore3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayTest = { 0, 5, 10, 15, 20, 25 };
+            var result = arrayTest[0..^3];
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            var text = "Microsoft anuncia C# 8.0";
+
+            string range = text[^24..^0];
+            Console.WriteLine(range);
+
+            string range3 = text[^14..];
+            Console.WriteLine(range3);
+
+            string range4 = text[..9];
+            Console.WriteLine(range4);
+
+            Console.ReadKey();
+        }
+    }
+}
